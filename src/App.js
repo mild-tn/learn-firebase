64d211fd6f57b771d79ea476d218bc3f30d78d72
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-27 22:38:58 +0700

    [UPDATE] refactoring code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MessageList from './Components/MessageList'
-import MessageBox from './Components/MessageBox'
-import firebase from "firebase"
-import { Row, Col} from 'antd';
-import Header from './Components/Header'
-import styled from 'styled-components'
+import MessageList from './Components/MessageList';
+import MessageBox from './Components/MessageBox';
+import firebase from 'firebase';
+import { Row, Col } from 'antd';
+import Header from './Components/Header';
+import styled from 'styled-components';
 
 const Nav = styled.nav`
   height: 50px;
-  background-color:#282c34;
-`
+  background-color: #282c34;
+`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     var config = {
-      apiKey: "AIzaSyDUuaZ48acPcjdLYkVTobsPA9H6TZhkvag",
-      authDomain: "massagebox-f32c9.firebaseapp.com",
-      databaseURL: "https://massagebox-f32c9.firebaseio.com",
-      projectId: "massagebox-f32c9",
-      storageBucket: "massagebox-f32c9.appspot.com",
-      messagingSenderId: "204814869660"
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DATABASE_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STOREGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
     };
     firebase.initializeApp(config);
   }
@@ -30,14 +29,14 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Nav >
-        <Col span={1}></Col>
-        <Col span={11}>
-          <Header title="Self Learning."  />
-        </Col>
-      </Nav>
+        <Nav>
+          <Col span={1} />
+          <Col span={11}>
+            <Header title="Your note." />
+          </Col>
+        </Nav>
         <Row>
-          <Col span={2}></Col>
+          <Col span={2} />
           <Col>
             <MessageList db={firebase} />
           </Col>
