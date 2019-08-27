33930100a227e2d013b5a14b5c8dd6128ecdd8b5
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-27 22:39:34 +0700

    [UPDATE] refactoring code

--- src/Components/MessageBox.js
+++ src/Components/MessageBox.js
@@ -1,59 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import trim from 'trim';
 import { Card } from 'antd';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const CardSpace = styled(Card)`
-  margin:50px;
-`
+  margin: 50px;
+`;
 
 const Textarea = styled.textarea`
   height: 100px;
-  border : 0px;
-`
+  border: 0px;
+`;
 class MessageBox extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
     this.onKeyup = this.onKeyup.bind(this);
     this.state = {
-      message: ''
+      message: '',
     };
   }
-  onChange(e){
+  onChange(e) {
     this.setState({
       message: e.target.value,
-
     });
   }
-  onKeyup(e){
-    if(e.keyCode === 13 && trim(e.target.value) !== ''){
+  onKeyup(e) {
+    if (e.keyCode === 13 && trim(e.target.value) !== '') {
       e.preventDefault();
       let dbCon = this.props.db.database().ref('/messages');
       dbCon.push({
-        message: trim(e.target.value)
+        message: trim(e.target.value),
       });
       this.setState({
-        message: ''
+        message: '',
       });
-    
     }
   }
 
-render() {
+  render() {
     return (
       <CardSpace>
-      <form style={{border : 0}}>
-       <Textarea
-          placeholder="Input message"
-          cols="100"
-          onChange={this.onChange}
-          onKeyUp={this.onKeyup}
-          value={this.state.message}>
-       </Textarea>
-      </form>
+        <form style={{ border: 0 }}>
+          <Textarea
+            placeholder="Input message"
+            cols="100"
+            onChange={this.onChange}
+            onKeyUp={this.onKeyup}
+            value={this.state.message}
+          />
+        </form>
       </CardSpace>
-    )
+    );
   }
 }
-export default MessageBox
+export default MessageBox;
