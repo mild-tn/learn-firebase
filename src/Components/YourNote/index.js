2df5043e79d1bc870c764e029714b75e3a9832b6
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-28 12:18:06 +0700

    [UPDATE] yournotes page

---
+++ src/Components/YourNote/index.js
@@ -0,0 +1,33 @@
+import React, { Component,Fragment } from 'react'
+import MessageList from '../Core/MessageList'
+import MessageBox from '../Core/MessageBox'
+import Nav from '../Core/Nav'
+import { Row, Col } from 'antd';
+
+
+export default class index extends Component {
+  render() {
+    const {firebase} = this.props
+    return (
+      <Fragment>
+        <Nav>
+          <Col span={1} />
+          <Col span={11}>
+            <Nav title="Your note." />
+          </Col>
+        </Nav>
+        <Row>
+          <Col span={2} />
+          <Col>
+            <MessageList db={firebase} />
+          </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col>
+            <MessageBox db={firebase} />
+          </Col>
+        </Row>
+      </Fragment>
+    )
+  }
+}
